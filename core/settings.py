755fd64049f39475be3245e389be882ff7e0538e
Mardochee Reveil <mardorev@gmail.com>
2021-08-14 23:58:21 -0400

    Add sentry integration

--- core/settings.py
+++ core/settings.py
@@ -147,3 +147,20 @@ CSRF_COOKIE_SECURE = False
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
+
+
+# Sentry
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=config("SENTRY_DSN", default=None),
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=0.50,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
