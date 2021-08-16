913cec796d6cc5116b03957641eab331b69d1c7e
Mardochee Reveil <mardorev@gmail.com>
2021-08-15 23:55:45 -0400

    Fix bug when site name is missing from database

--- core/context_processors.py
+++ core/context_processors.py
@@ -3,5 +3,8 @@ from django.http import HttpRequest
 
 
 def access_site_name(HttpRequest):
-
-    return {"site_name": KeyValuePair.objects.filter(key="site_name")[0].value}
+    site_name = "Disaster Monitoring"
+    site_name_query = KeyValuePair.objects.filter(key="site_name")
+    if len(site_name_query) > 0:
+        site_name = site_name_query[0].value
+    return {"site_name": site_name}
