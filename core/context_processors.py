6e03e21f998e9c9aa03af14813ca31ce407c7ca2
Mardochee Reveil <mardorev@gmail.com>
2021-08-18 18:41:33 -0400

    Add Google Analytics

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,6 @@
 from app.models import KeyValuePair
 from django.http import HttpRequest
+from decouple import config
 
 
 def access_site_name(HttpRequest):
@@ -7,4 +8,4 @@ def access_site_name(HttpRequest):
     site_name_query = KeyValuePair.objects.filter(key="site_name")
     if len(site_name_query) > 0:
         site_name = site_name_query[0].value
-    return {"site_name": site_name}
+    return {"site_name": site_name, "GOOGLE_ANALYTICS_TAG": config("GOOGLE_ANALYTICS")}
