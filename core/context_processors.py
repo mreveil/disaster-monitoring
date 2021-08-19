3a3b1d1c31730d1b6455f7812d80c64f72005987
Mardochee Reveil <mardorev@gmail.com>
2021-08-18 20:01:14 -0400

    Fix bug when a Google analytics tag is not provided

--- core/context_processors.py
+++ core/context_processors.py
@@ -8,4 +8,7 @@ def access_site_name(HttpRequest):
     site_name_query = KeyValuePair.objects.filter(key="site_name")
     if len(site_name_query) > 0:
         site_name = site_name_query[0].value
-    return {"site_name": site_name, "GOOGLE_ANALYTICS_TAG": config("GOOGLE_ANALYTICS")}
+    return {
+        "site_name": site_name,
+        "GOOGLE_ANALYTICS_TAG": config("GOOGLE_ANALYTICS", default=None),
+    }
